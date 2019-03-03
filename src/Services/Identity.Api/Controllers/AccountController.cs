0f9e1db7d254f20d21424a66d7590511b6ff1dc9
Process My Media <mnicolescu@gmail.com>
2019-03-03 18:15:44 +0100

    Add Identity.Api project

---
+++ src/Services/Identity.Api/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿namespace Identity.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Account Controller
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    public class AccountController : Controller
+    {
+    }
+}
