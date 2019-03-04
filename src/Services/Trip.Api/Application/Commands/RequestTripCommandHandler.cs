7cb349f700bd4f3c8e5909a621dcee138bd836a9
mnicolescu <mnicolescu@WDEHQPAR1-00042>
2019-03-04 17:43:22 +0100

    Add RequestTripCommandHandler

---
+++ src/Services/Trip.Api/Application/Commands/RequestTripCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Trip.Api.Application.Commands
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MediatR;
+
+    /// <summary>
+    /// Request Trip Command Handler
+    /// </summary>
+    /// <seealso cref="MediatR.IRequestHandler{Trip.Api.Application.Commands.RequestTripCommand, System.Boolean}" />
+    public class RequestTripCommandHandler : IRequestHandler<RequestTripCommand, bool>
+    {
+        /// <summary>
+        /// Handles the specified request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task<bool> Handle(RequestTripCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
