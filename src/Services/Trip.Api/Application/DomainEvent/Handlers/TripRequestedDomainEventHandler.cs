822ab554899ef757ad2790450db3d92a32135913
Process My Media <mnicolescu@gmail.com>
2019-03-10 16:02:17 +0100

    Add TripRequestedDomainEventHandler

---
+++ src/Services/Trip.Api/Application/DomainEvent/Handlers/TripRequestedDomainEventHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Trip.Api.Application.DomainEvent.Handlers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MediatR;
+
+    /// <summary>
+    /// TripRequestedDomainEvent Handler
+    /// </summary>
+    /// <seealso cref="MediatR.INotificationHandler{Trip.Domain.TripRequestedDomainEvent}" />
+    public class TripRequestedDomainEventHandler : INotificationHandler<Domain.TripRequestedDomainEvent>
+    {
+        /// <summary>
+        /// Handles the specified notification.
+        /// </summary>
+        /// <param name="notification">The notification.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public Task Handle(Domain.TripRequestedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
