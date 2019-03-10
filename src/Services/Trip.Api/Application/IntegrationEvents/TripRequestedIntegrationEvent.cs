1717817ca2876bb9bc25cdf4a5c75ac5198e07bb
Process My Media <mnicolescu@gmail.com>
2019-03-10 16:46:20 +0100

    Add TripRequestedIntegrationEvent class

---
+++ src/Services/Trip.Api/Application/IntegrationEvents/TripRequestedIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Trip.Api.Application.IntegrationEvents
+{
+    /// <summary>
+    /// TODO : implement factory integration event
+    /// </summary>
+    public class TripRequestedIntegrationEvent
+    {
+    }
+}
